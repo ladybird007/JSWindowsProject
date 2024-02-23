f69a6dc2b6da6fd2863fd54aaeb4c7cfe8e145c8
ishunter216 <michael.king@edenprogram.com>
2024-02-23 16:11:01 +0200

    modals - next step

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,12 @@ import tabs from './modules/tabs';
 import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
+
+    let modalState = {};
+    
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block')
     forms();
 })
