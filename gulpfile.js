8d125621c3813eb367557582224ec4c143933162
ishunter216 <michael.king@edenprogram.com>
2024-02-27 12:32:41 +0200

    gulp prod js

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-// const dist = "./dist/";
-const dist = "C:/MAMP/htdocs/windows";
+const dist = "./dist/";
+// const dist = "C:/MAMP/htdocs/windows";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
